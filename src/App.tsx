3b361d316ab80604c4bb84c4c7e9a30d8011a9c7
vctrhugoop <vhdestroy@hotmail.com>
2024-04-22 21:43:03 -0300

    🌈 style: add hidden header mobile

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import { CardInfo } from './components/CardInfos';
 export function App() {
   return (
     <div>
-      <header className="bg-[url('/src/assets/Cover.png')] h-[292px] bg-cover bg-center"></header>
+      <header className="bg-[url('/src/assets/Cover.png')] h-[292px] bg-cover bg-center hidden md:block"></header>
       <div className='px-4'>
         <CardInfo />
+        <main></main>
       </div>
     </div>
   );
