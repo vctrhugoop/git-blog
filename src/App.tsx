5851d9406a240edffea8bc3f7f7f598378528313
vctrhugoop <vhdestroy@hotmail.com>
2024-04-24 19:36:17 -0300

    feat: add  logo in header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { Outlet } from 'react-router-dom';
+import Logo from './assets/Logo.png';
 
 export function App() {
   return (
     <div>
-      <header className="bg-[url('/src/assets/Cover.png')] h-[292px] bg-cover bg-center hidden md:block" />
+      <header className="bg-[url('/src/assets/Cover.png')] h-[292px] bg-cover bg-center hidden md:flex items-center justify-center">
+        <img src={Logo} className='mb-12' />
+      </header>
       <Outlet />
     </div>
   );
