76ef72a00664db31a925c908597fbba4c87b6e4d
vctrhugoop <vhdestroy@hotmail.com>
2024-04-23 21:55:55 -0300

    ✨ feat: add router

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './App.tsx';
+import { BrowserRouter } from 'react-router-dom';
+import { Router } from './Router.tsx';
 import { GitHubDataContextProvider } from './contexts/GitHubDataContext.tsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <GitHubDataContextProvider>
-      <App />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </GitHubDataContextProvider>
   </React.StrictMode>,
 );
