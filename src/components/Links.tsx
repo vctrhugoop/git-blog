8bf9634e9ca80dca4e7d44d9b958ab717ded65cb
vctrhugoop <vhdestroy@hotmail.com>
2024-04-26 18:08:09 -0300

    ✨ feat: add rest props

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -1,18 +1,18 @@
-import { ReactNode } from 'react';
+import { HTMLAttributes, ReactNode } from 'react';
+import { Link, LinkProps } from 'react-router-dom';
 
 type LinksProps = {
-  url: string;
   children: ReactNode;
-};
+} & Omit<HTMLAttributes<HTMLAnchorElement>, 'href'> &
+  LinkProps;
 
-export function Links({ url, children }: LinksProps) {
+export function Links({ children, ...props }: LinksProps) {
   return (
-    <a
-      href={url}
-      target='_blank'
-      className='flex items-center gap-2 text-base-blue font-bold text-xs hover:border-b-base-blue hover:border-b'
+    <Link
+      {...props}
+      className='flex items-center gap-2 text-base-blue font-bold text-xs border-b border-base-profile hover:border-b-base-blue hover:border-b'
     >
       {children}
-    </a>
+    </Link>
   );
 }
